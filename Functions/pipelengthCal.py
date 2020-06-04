eedd05fa1f5caa1628e9dcb1b3cae01b366afd80
christopher <ucemcog@ucl.ac.uk>
2020-06-05 00:54:34 +0100

    Fix bugs

--- Functions/pipelengthCal.py
+++ Functions/pipelengthCal.py
@@ -16,7 +16,7 @@ def calculatePipeLength():
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
 
-    driver=webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options)
+    driver=webdriver.Chrome(executable_path=str(os.environ.get("CHROMEDRIVER_PATH")),chrome_options=chrome_options)
 
     #driver = webdriver.Chrome(executable_path="chromedriver.exe")
 
